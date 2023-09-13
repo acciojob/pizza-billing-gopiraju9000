2ae771e698650f984c1786e254701e7ac0bb9d9a
gopiraju000555 <rajumaka911@gmail.com>
2023-09-13 15:44:44 +0530

    pizza bill

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,5 +5,12 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+        extraCheese = 0;
+        extraToppings = 0;
+        addTakeaway();
+        addExtraToppings();
+        addExtraCheese();
+
+
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,30 +5,59 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
-
+    public int extraToppings;
+    public int extraCheese = 80;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            price = price + 300;//add the pizza pric
+            extraToppings = 70;
+            bill += "Base Price of The Pizza = 300 " + "\n";
+        }
+        else{
+            extraToppings = 120;
+            price = price + 400;
+            bill = bill + "Base price of The Pizza = 400  " + "\n";
+        }
     }
 
     public int getPrice(){
+
         return this.price;
     }
 
     public void addExtraCheese(){
+
         // your code goes here
+        price = price + extraCheese;
+        bill = bill + "Extra cheese price = "+extraCheese + "\n";
+
     }
 
     public void addExtraToppings(){
+
         // your code goes here
+
+        if(isVeg) {
+            bill = bill + "Extra Toppings Added = 70"+ "\n";
+            price = price + extraToppings;
+        }
+        else{
+            bill = bill + "Extra Toppings Added = 120" + "\n";
+            price = price + extraToppings;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        price = price + 20;
+        bill = bill + "Paperbag Added = 20"+"\n";
     }
 
     public String getBill(){
         // your code goes here
+        bill = bill + "Total Price : "+price + "\n";
         return this.bill;
     }
 }
