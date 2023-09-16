0ef7e971f8ee31e50f539a795eb08dda62986568
gopiraju000555 <rajumaka911@gmail.com>
2023-09-16 11:34:26 +0530

    pizza

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -14,5 +14,26 @@ public class Main {
     p.addExtraToppings();
     p.addTakeaway();
     System.out.println(p.getBill());
+
+    Pizza dp1 = new Pizza(false);
+    dp1.addExtraToppings();
+    dp1.addExtraCheese();
+    dp1.addTakeaway();
+    System.out.println(dp1.getBill());
+
+    Pizza dp2 = new Pizza(false);
+    dp2.addExtraToppings();
+    dp2.addExtraCheese();
+    dp2.addTakeaway();
+    System.out.println(dp2.getBill());
+
+
+    Pizza q = new Pizza(true);
+    q.addExtraCheese();
+    q.addExtraToppings();
+    q.addTakeaway();
+    System.out.println(q.getBill());
+
+
   }
 }
